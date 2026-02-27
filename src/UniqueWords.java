b25ed339c288744c1a3cbff5deae7e94032e61c2
Lukas Lozada Perez <lukas.lozada24@my.stjohns.edu>
2026-02-26 22:43:37 -0500

    Completed Lab 2

--- src/UniqueWords.java
+++ src/UniqueWords.java
@@ -7,31 +7,38 @@ public class UniqueWords
 		@param list ArrayList of strings to be examined
 		@return number of unique strings in the list
    */
-   public static int countUnique(ArrayList<String> list)
-   {
-	  int count = 0;
-	  
-      for (int i = 0; i < list.size(); i++)
-      {		 for (int j = 0; j < list.size(); j++)
-		 {
-			
-		 }
+
+   public static int countUnique(ArrayList<String> list) {
+      int count = 0;
+
+      for (int i = 0; i < list.size(); i++) {
+         int hadDuplicatesBefore = 0;
+
+         for (int j = 0; j < i; j++) {
+            if (list.get(i).equals(list.get(j))) {
+               hadDuplicatesBefore++;
+            }
+         }
+
+         if (hadDuplicatesBefore == 0) {
+            count++;
+         }
       }
-	  return count;
+
+      return count;
    }
 
-   public static void main(String[] args)
-   {
-      ArrayList <String> words = new ArrayList<>();
-      words.add("apple");
-	  words.add("orange");
-	  words.add("blackboard");
-	  words.add("apple");
-	  words.add("orange");
-	  words.add("sun");
-	  words.add("moon");
-   
-	  int unique = countUnique(words);
-      System.out.println(words + " has " + unique + " unique words");
+   // stays the same
+public static void main(String[] args) {
+   ArrayList <String> words = new ArrayList<>();
+         words.add("apple");
+         words.add("orange");
+         words.add("blackboard");
+         words.add("apple");
+         words.add("orange");
+         words.add("sun");
+         words.add("moon");
+         int unique = countUnique(words);
+      System.out.println(words + " has: " + unique + " unique words.");
    }
 }
